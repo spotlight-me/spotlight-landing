bd6b7a304a9f3c79a70184de8fdcf7225ada48e2
Abel Dagne <a.dagne16@gmail.com>
2024-04-11 23:50:21 -0700

    init

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Nav from './Nav';
 import { Application } from '@splinetool/runtime';

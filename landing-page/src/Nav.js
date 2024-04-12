0e12a70ba80147978d62dc9fbb58d9999a49e9ec
Sheden Andemicael <shedenandemicael@DNa1de26d.SUNet>
2024-04-11 17:21:28 -0700

    updated logo and nav

--- landing-page/src/Nav.js
+++ landing-page/src/Nav.js
@@ -4,7 +4,12 @@ import SpotlightLogo from './SpotlightLogo.png'
 const Nav = () => {
   return (
     <nav className='Nav'>
+        <div className='left-nav-items'>
         <img src={SpotlightLogo} className="spotlight-logo" alt="spotlight logo" />
+        </div>
+        <div className='right-nav-items'>
+            
+        </div>
     </nav>
   )
 }

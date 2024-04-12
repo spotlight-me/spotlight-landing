80a10ee211a48a243008c1e619a77e4118991639
Sheden Andemicael <shedenandemicael@DNa1de26d.SUNet>
2024-04-11 20:52:32 -0700

    updated nav bar

--- landing-page/src/Nav.js
+++ landing-page/src/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import SpotlightLogo from './SpotlightLogo.png'
+import SpotlightLogo from './SpotlightLogo.svg'
 
 const Nav = () => {
   return (
     <nav className='Nav'>
-        <div className='left-nav-items'>
+        <div className='left-nav-item-container'>
         <img src={SpotlightLogo} className="spotlight-logo" alt="spotlight logo" />
         </div>
-        <div className='right-nav-items'>
-            
+        <div className='right-nav-item-container'>
+            <h1 className='right-nav-item inter-font'>FAQs</h1>
+            <h1 className='right-nav-item inter-font'>Join The Waitlist</h1>
         </div>
     </nav>
   )

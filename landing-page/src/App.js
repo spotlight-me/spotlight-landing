110f372608964343adeab5ecd7f033e56a2f4cee
Abel Dagne <a.dagne16@gmail.com>
2024-04-12 10:06:44 -0700

    Revert "Updates"
    This reverts commit 5c76a8eff887503a8320e96a037037abe8b63182.

---
+++ landing-page/src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import FAQ from './FAQ';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
